4f46de29d06eb78a30a7a04e53be819385d62f02
whitecy01 <whitecy01@naver.com>
2025-12-24 15:42:00 +0900

    feat : java의 정석 - 기초편 챕터 1~5

---
+++ book/javaEssentialsBasics/Chapter04/Ex4_7.java
@@ -0,0 +1,12 @@
+package Chapter04;
+
+class Ex4_7 {
+	public static void main(String args[]) {
+		int num = 0;
+
+		for (int i = 1; i <= 5; i++) {
+			num = (int) (Math.random() * 6) + 1;
+			System.out.println(num);
+		}
+	}
+}
