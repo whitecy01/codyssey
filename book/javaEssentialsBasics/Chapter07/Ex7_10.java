35397abd922cfcca396cf5de3d763c43161d31fb
whitecy01 <whitecy01@naver.com>
2025-12-27 16:18:34 +0900

    feat : 혼자공부하는 자바 챕터 7까지 정리

---
+++ book/javaEssentialsBasics/Chapter07/Ex7_10.java
@@ -0,0 +1,38 @@
+package Chapter07;
+
+public class Ex7_10 {
+	public static void main(String[] args) {
+		Unit[] group = { new Marine(), new Tank(), new Dropship() };
+
+		for (int i = 0; i < group.length; i++)
+			group[i].move(100, 200);
+	}
+}
+
+abstract class Unit {
+	int x, y;
+	abstract void move(int x, int y);
+	void stop() { /* ���� ��ġ�� ���� */ }
+}
+
+class Marine extends Unit { // ����
+	void move(int x, int y) {
+		System.out.println("Marine[x=" + x + ",y=" + y + "]");
+	}
+	void stimPack() { /* �������� ����Ѵ�. */ }
+}
+
+class Tank extends Unit { // ��ũ
+	void move(int x, int y) {
+		System.out.println("Tank[x=" + x + ",y=" + y + "]");
+	}
+	void changeMode() { /* ���ݸ�带 ��ȯ�Ѵ�. */ }
+}
+
+class Dropship extends Unit { // ���ۼ�
+	void move(int x, int y) {
+		System.out.println("Dropship[x=" + x + ",y=" + y + "]");
+	}
+	void load()   { /* ���õ� ����� �¿��. */ }
+	void unload() { /* ���õ� ����� ������. */ }
+}
